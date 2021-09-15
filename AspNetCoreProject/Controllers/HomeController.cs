51ebb2d1086ecb8f98a79aa5e61f612a5bb61bbb
Chingiz <chin1296@yandex.ru>
2021-09-15 23:01:03 +0300

    Создание первого контроллера

---
+++ AspNetCoreProject/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => Content("FFF");
+        public IActionResult SecondAction() => Content("SSS");
+    }
+}
