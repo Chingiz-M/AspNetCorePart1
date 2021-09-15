4335b23b7e489ec0e07955ae8066ff7091fb9e39
Chingiz <chin1296@yandex.ru>
2021-09-15 22:39:50 +0300

    Работа с файлом конфига

--- AspNetCoreProject/Startup.cs
+++ AspNetCoreProject/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,6 +9,13 @@ namespace AspNetCoreProject
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -25,7 +33,7 @@ namespace AspNetCoreProject
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(Configuration["Hello"]);
                 });
             });
         }
