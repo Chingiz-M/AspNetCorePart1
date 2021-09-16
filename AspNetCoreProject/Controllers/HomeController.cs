2da67c4b96e6e3f5df9f10a6dc84cf971a8f4939
Chingiz <chin1296@yandex.ru>
2021-09-16 23:39:45 +0300

    Создание представления сотрудников

--- AspNetCoreProject/Controllers/HomeController.cs
+++ AspNetCoreProject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreProject.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,13 @@ namespace AspNetCoreProject.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> employees = new List<Employee>()
+        {
+            new Employee(1,"Ivan","Ivanov","Ivanovich",19),
+            new Employee(1,"Petr","Petrov","Petrovich",32)
+        };
         public IActionResult Index() => View();
         public IActionResult SecondAction() => Content("SSS");
+        public IActionResult Employees() => View(employees);
     }
 }
