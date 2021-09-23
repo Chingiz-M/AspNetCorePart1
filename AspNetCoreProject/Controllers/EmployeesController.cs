b9432622ae6e17092f41015a44ea894bea7b90e0
Chingiz <chin1296@yandex.ru>
2021-09-23 16:43:15 +0300

    Выделение тестовых данных в отдельный класс

--- AspNetCoreProject/Controllers/EmployeesController.cs
+++ AspNetCoreProject/Controllers/EmployeesController.cs
@@ -1,19 +1,18 @@
-﻿using AspNetCoreProject.Models;
+﻿using AspNetCoreProject.Data;
+using AspNetCoreProject.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AspNetCoreProject.Controllers
 {
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> employees = new List<Employee>()
+        private readonly IEnumerable<Employee> _Employees;
+        public EmployeesController()
         {
-            new Employee(1,"Ivan","Ivanov","Ivanovich",19,new DateTime(2002,12,3), "5 лет"),
-            new Employee(2,"Petr","Petrov","Petrovich",31,new DateTime(1990,11,4), "6 лет")
-        };
-        public IActionResult Index() => View(employees);
+            _Employees = TestData.Employees;
+        }
+
+        public IActionResult Index() => View(_Employees);
     }
 }
