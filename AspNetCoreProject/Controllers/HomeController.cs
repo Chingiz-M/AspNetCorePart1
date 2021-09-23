68e8550891276e489f138ac38338eeae3a5d5746
Chingiz <chin1296@yandex.ru>
2021-09-23 19:27:27 +0300

    Добавление остальных представлений

--- AspNetCoreProject/Controllers/HomeController.cs
+++ AspNetCoreProject/Controllers/HomeController.cs
@@ -5,6 +5,14 @@ namespace AspNetCoreProject.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
-
+        public IActionResult Error404() => View();
+        public IActionResult Blog() => View();
+        public IActionResult BlogSingle() => View();
+        public IActionResult Cart() => View();
+        public IActionResult Checkout() => View();
+        public IActionResult ContactUs() => View();
+        public IActionResult Login() => View();
+        public IActionResult ProductDetails() => View();
+        public IActionResult Shop() => View();
     }
 }
