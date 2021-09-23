4a9cd3780564a0bc42574d1bb9b901599a806eef
Chingiz <chin1296@yandex.ru>
2021-09-23 16:35:29 +0300

    Создание контроллера Сотрудники

---
+++ AspNetCoreProject/Controllers/EmployeesController.cs
@@ -0,0 +1,19 @@
+﻿using AspNetCoreProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreProject.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> employees = new List<Employee>()
+        {
+            new Employee(1,"Ivan","Ivanov","Ivanovich",19,new DateTime(2002,12,3), "5 лет"),
+            new Employee(2,"Petr","Petrov","Petrovich",31,new DateTime(1990,11,4), "6 лет")
+        };
+        public IActionResult Index() => View(employees);
+    }
+}
