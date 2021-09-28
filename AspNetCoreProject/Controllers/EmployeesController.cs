f8b489456e891c253752acbe89f21a56a2ec9652
Chingiz <chin1296@yandex.ru>
2021-09-28 22:31:04 +0300

    Добавление ViewModel Employee

--- AspNetCoreProject/Controllers/EmployeesController.cs
+++ AspNetCoreProject/Controllers/EmployeesController.cs
@@ -1,29 +1,47 @@
-﻿using AspNetCoreProject.Data;
-using AspNetCoreProject.Models;
+﻿using AspNetCoreProject.Services.Interfaces;
+using AspNetCoreProject.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.Extensions.Logging;
 
 namespace AspNetCoreProject.Controllers
 {
     public class EmployeesController : Controller
     {
-        private readonly IEnumerable<Employee> _Employees;
-        public EmployeesController()
+        private readonly IEmployeesData employeesData;
+        private readonly ILogger<EmployeesController> logger;
+
+        public EmployeesController(IEmployeesData EmployeesData, ILogger<EmployeesController> Logger)
         {
-            _Employees = TestData.Employees;
+            employeesData = EmployeesData;
+            logger = Logger;
         }
 
-        public IActionResult Index() => View(_Employees);
+        public IActionResult Index() => View(employeesData.GetAll());
 
         public IActionResult Details(int id)
         {
-            var employee = _Employees.SingleOrDefault(e => e.Id == id);
+            var employee = employeesData.GetById(id);
 
             if (employee == null)
                 return NotFound();
 
             return View(employee);
         }
+
+        public IActionResult Edit(int id)
+        {
+            var employee = employeesData.GetById(id);
+
+            if (employee == null)
+                return NotFound();
+
+            var model = new EmployeeViewModel(employee.Id,
+                employee.Name, employee.Lastname, employee.Surname);
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel model)
+        {
+
+        }
     }
-}
