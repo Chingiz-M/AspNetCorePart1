2294e647be6c363ee3f0287b91f7fa20362918d0
Chingiz <chin1296@yandex.ru>
2021-09-28 21:30:22 +0300

    Создание интерфейса сервиса сотрудника

---
+++ AspNetCoreProject/Services/Interfaces/IEmployeesData.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCoreProject.Models;
+using System.Collections.Generic;
+
+namespace AspNetCoreProject.Services.Interfaces
+{
+    interface IEmployeesData
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int id);
+        int Add(Employee employee);
+        void Update(Employee employee);
+        bool Delete(int id);
+    }
+}
