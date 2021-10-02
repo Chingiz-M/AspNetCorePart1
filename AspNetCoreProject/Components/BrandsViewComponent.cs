efa9999cddc8c90e0f3b0e418d80a242eeef628d
Chingiz <chin1296@yandex.ru>
2021-10-02 17:26:11 +0300

    Добавление логики визуальныого компонента брендов

--- AspNetCoreProject/Components/BrandsViewComponent.cs
+++ AspNetCoreProject/Components/BrandsViewComponent.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreProject.Data;
+using AspNetCoreProject.Services.Interfaces;
+using AspNetCoreProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace AspNetCoreProject.Components
 {
     public class BrandsViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
+        private readonly IProductData productData;
+
+        public BrandsViewComponent(IProductData ProductData)
+        {
+            productData = ProductData;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var brands = productData.GetBrands().OrderBy(b => b.Order).Select(
+                b => new BrandViewModel
+                {
+                    Id = b.Id,
+                    Name = b.Name,
+                });
+            return View(brands);
+        }
     }
 }
