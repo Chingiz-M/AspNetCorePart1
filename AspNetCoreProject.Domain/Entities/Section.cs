42348984d33837a6c6b030e966fa47ad1115a328
Chingiz <chin1296@yandex.ru>
2021-10-02 17:30:47 +0300

    Добавление класса товара

--- AspNetCoreProject.Domain/Entities/Section.cs
+++ AspNetCoreProject.Domain/Entities/Section.cs
@@ -8,4 +8,12 @@ namespace AspNetCoreProject.Domain.Entities
         public int Order { get; set; }
         public int? ParentId { get; set; }
     }
+    public class Product : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+        public int SectionId { get; set; }
+        public int? BrandId { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
 }
