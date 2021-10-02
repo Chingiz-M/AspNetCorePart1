3b6e3a315ccd5c568c0981e2814e2d3000f70e05
Chingiz <chin1296@yandex.ru>
2021-10-02 12:35:28 +0300

    Добавление визуальных аттрибутов в модель представлений сотрудников

--- AspNetCoreProject/ViewModels/EmployeeViewModel.cs
+++ AspNetCoreProject/ViewModels/EmployeeViewModel.cs
@@ -1,16 +1,22 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreProject.ViewModels
 {
     public class EmployeeViewModel
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
+        [Display(Name = "Имя")]
         public string Name { get; set; }
+        [Display(Name = "Отчество")]
         public string Lastname { get; set; }
+        [Display(Name = "Фамилия")]
         public string Surname { get; set; }
+        [Display(Name = "Возраст")]
         public int Age { get; set; }
     }
 }
