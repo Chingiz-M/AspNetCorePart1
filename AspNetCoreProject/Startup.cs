b40b693f67b33787d485c760fafe2b4423e0d7bc
Chingiz <chin1296@yandex.ru>
2021-10-03 18:49:54 +0300

    Обработка ошибки 404

--- AspNetCoreProject/Startup.cs
+++ AspNetCoreProject/Startup.cs
@@ -30,6 +30,8 @@ namespace AspNetCoreProject
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStatusCodePagesWithRedirects("~/home/Status/{0}");
+
             app.UseStaticFiles();
 
             app.UseRouting();
