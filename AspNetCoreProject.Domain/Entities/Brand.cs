4020fd87089eeb25e660ee44a57542c5333d6098
Chingiz <chin1296@yandex.ru>
2021-10-03 19:08:30 +0300

    Доработка сущностей для использования в EF Core

--- AspNetCoreProject.Domain/Entities/Brand.cs
+++ AspNetCoreProject.Domain/Entities/Brand.cs
@@ -1,8 +1,10 @@
 ﻿using AspNetCoreProject.Domain.Entities.Base;
 using AspNetCoreProject.Domain.Entities.Base.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreProject.Domain.Entities
 {
+    [Index(nameof(Name), IsUnique = true)]
     public class Brand : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
--- AspNetCoreProject.Domain/Entities/Section.cs
+++ AspNetCoreProject.Domain/Entities/Section.cs
@@ -1,11 +1,18 @@
 ﻿using AspNetCoreProject.Domain.Entities.Base;
 using AspNetCoreProject.Domain.Entities.Base.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AspNetCoreProject.Domain.Entities
 {
+    [Index(nameof(Name), IsUnique = true)]
     public class Section : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
         public int? ParentId { get; set; }
+        [ForeignKey(nameof(ParentId))]
+        public Section Parent { get; set; }
+        public ICollection<Product> Products { get; set; } 
     }
 }
