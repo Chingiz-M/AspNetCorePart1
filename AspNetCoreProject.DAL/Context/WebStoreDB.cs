a816216591b0adf3fe6e691c1efd260e4ffefb80
Chingiz <chin1296@yandex.ru>
2021-10-03 19:19:34 +0300

    Определение контекста данных БД

---
+++ AspNetCoreProject.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,21 @@
+﻿using AspNetCoreProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreProject.DAL.Context
+{
+    class WebStoreDB: DbContext
+    {
+        DbSet<Product> Products { get; set; }
+        DbSet<Section> Sections { get; set; }
+        DbSet<Brand> Brands { get; set; }
+        public WebStoreDB(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
