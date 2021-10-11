c2021dd732bb1ad85863cefb4ec127c62188750f
Chingiz <chin1296@yandex.ru>
2021-10-11 22:16:18 +0300

    Заготовка контроллера account

---
+++ AspNetCoreProject/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register => View();
+        public IActionResult Login => View();
+        public IActionResult Logout => RedirectToAction("Index", "Home");
+        public IActionResult AccsessDenied => View();
+    }
+}
