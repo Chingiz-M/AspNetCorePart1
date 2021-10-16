0c6f52a2f330f8fd03caecea55d889991ce46ade
Chingiz <chin1296@yandex.ru>
2021-10-16 20:48:50 +0300

    Модель представления корзины

--- AspNetCoreProject/ViewModels/CartViewModel.cs
+++ AspNetCoreProject/ViewModels/CartViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace AspNetCoreProject.ViewModels
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreProject.ViewModels
 {
     public class CartViewModel
     {
+        public IEnumerable<(ProductViewModel product, int Quantity)> Items { get; set; }
+        public int ItemsSum => Items?.Sum(i => i.Quantity) ?? 0;
+        public decimal TotalSum => Items?.Sum(i => i.product.Price * i.Quantity) ?? 0;
     }
 }
