cdd3b2f5af24656ca45aee91e71609f8c85532de
Chingiz <chin1296@yandex.ru>
2021-10-18 20:58:27 +0300

    Вывод всех товаров в каталоге управления товаров области админа

--- AspNetCoreProject/Areas/Admin/Controllers/ProductsController.cs
+++ AspNetCoreProject/Areas/Admin/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreProject.Infrastructure.Mapping;
+using AspNetCoreProject.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,19 @@ namespace AspNetCoreProject.Areas.Admin.Controllers
     [Area("Admin")]
     public class ProductsController : Controller
     {
+        private readonly IProductData productData;
+
+        public ProductsController(IProductData productData)
+        {
+            this.productData = productData;
+        }
         public IActionResult Index()
         {
-            return View();
+            var products = productData.GetProducts().ToView();
+            return View(products);
         }
+
+        public IActionResult Edit(int id) => RedirectToAction(nameof(Index));
+        public IActionResult Remove(int id) => RedirectToAction(nameof(Index));
     }
 }
