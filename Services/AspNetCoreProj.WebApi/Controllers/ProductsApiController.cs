23dbb5f814f048f73f3392ec5341e75150dc9582
Chingiz <chin1296@yandex.ru>
2021-10-27 23:52:44 +0300

    Доработка контроллера каталога товаров WebApi с помощью DTO моделей

--- Services/AspNetCoreProj.WebApi/Controllers/ProductsApiController.cs
+++ Services/AspNetCoreProj.WebApi/Controllers/ProductsApiController.cs
@@ -1,4 +1,5 @@
 ﻿using AspNetCoreProject.Domain;
+using AspNetCoreProject.Domain.DTO;
 using AspNetCoreProject.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -23,31 +24,31 @@ namespace AspNetCoreProj.WebApi.Controllers
         public IActionResult GetSections()
         {
             var sections = productData.GetSections();
-            return Ok(sections);
+            return Ok(sections.ToDTO());
         }
         [HttpGet("sections/{id}")]
         public IActionResult GetSectionById(int id)
         {
             var section = productData.GetSectionById(id);
-            return Ok(section);
+            return Ok(section.ToDTO());
         }
         [HttpGet("brands")]
         public IActionResult GetBrands()
         {
             var brands = productData.GetBrands();
-            return Ok(brands);
+            return Ok(brands.ToDTO());
         }
         [HttpGet("brands/{id}")]
         public IActionResult GetBrandsById(int id)
         {
             var brand = productData.GetBrandById(id);
-            return Ok(brand);
+            return Ok(brand.ToDTO());
         }
         [HttpPost]
         public IActionResult GetProducts(ProductFilter filter = null)
         {
             var products = productData.GetProducts(filter);
-            return Ok(products);
+            return Ok(products.ToDTO());
         }
         [HttpGet("{id}")]
         public IActionResult GetProductById(int id)
@@ -55,7 +56,7 @@ namespace AspNetCoreProj.WebApi.Controllers
             var product = productData.GetProductById(id);
             if (product is null)
                 return NotFound();
-            return Ok(product);
+            return Ok(product.ToDTO());
         }
     }
 }
