2729db24eef935e853aac0e0e951726d3b02c8eb
Chingiz <chin1296@yandex.ru>
2021-10-31 18:55:50 +0300

    Интерфейсы для WebApi системы Identity

---
+++ Services/AspNetCoreProj.Interfaces/Services/Identity/IUsersClient.cs
@@ -0,0 +1,16 @@
+﻿using AspNetCoreProject.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreProj.Interfaces.Services.Identity
+{
+    interface IUsersClient : 
+        IUserRoleStore<User>,
+        IUserPasswordStore<User>,
+        IUserPhoneNumberStore<User>,
+        IUserEmailStore<User>,
+        IUserTwoFactorStore<User>,
+        IUserLoginStore<User>,
+        IUserClaimStore<User>
+    {
+    }
+}
