a517862e53c5ecc1a6db22adc6c8a6b35dac7d47
Chingiz <chin1296@yandex.ru>
2021-10-31 20:51:07 +0300

    Загатовка логики контроллеров

--- Services/AspNetCoreProj.WebApi/Controllers/Identity/RolesApiController.cs
+++ Services/AspNetCoreProj.WebApi/Controllers/Identity/RolesApiController.cs
@@ -1,5 +1,11 @@
 ﻿using AspNetCoreProj.Interfaces;
+using AspNetCoreProject.DAL.Context;
+using AspNetCoreProject.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AspNetCoreProj.WebApi.Controllers.Identity
 {
@@ -7,5 +13,12 @@ namespace AspNetCoreProj.WebApi.Controllers.Identity
     [ApiController]
     public class RolesApiController : ControllerBase
     {
+        private readonly RoleStore<Role> _RoleStore;
+        public RolesApiController(WebStoreDB dB)
+        {
+            _RoleStore = new RoleStore<Role>(dB);
+        }
+        [HttpGet("all")]
+        public async Task<IEnumerable<Role>> GetAll() => await _RoleStore.Roles.ToArrayAsync();
     }
 }
--- Services/AspNetCoreProj.WebApi/Controllers/Identity/UsersApiController.cs
+++ Services/AspNetCoreProj.WebApi/Controllers/Identity/UsersApiController.cs
@@ -1,5 +1,11 @@
 ﻿using AspNetCoreProj.Interfaces;
+using AspNetCoreProject.DAL.Context;
+using AspNetCoreProject.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AspNetCoreProj.WebApi.Controllers.Identity
 {
@@ -7,5 +13,12 @@ namespace AspNetCoreProj.WebApi.Controllers.Identity
     [ApiController]
     public class UsersApiController : ControllerBase
     {
+        private readonly UserStore<User, Role, WebStoreDB> _UserStrore;
+        public UsersApiController(WebStoreDB dB)
+        {
+            _UserStrore = new UserStore<User, Role, WebStoreDB>(dB);
+        }
+        [HttpGet("all")]
+        public async Task<IEnumerable<User>> GetAll() => await _UserStrore.Users.ToArrayAsync();
     }
 }
