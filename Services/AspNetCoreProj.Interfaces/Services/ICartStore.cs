b7c0fef740c035fb3fc08e53f89f6ae93b0042ec
Chingiz <chin1296@yandex.ru>
2021-11-10 00:11:14 +0300

    Создание модульных тестов сервиса корзины

---
+++ Services/AspNetCoreProj.Interfaces/Services/ICartStore.cs
@@ -0,0 +1,9 @@
+﻿using AspNetCoreProject.Domain.Entities;
+
+namespace AspNetCoreProj.Interfaces.Services
+{
+    public interface ICartStore
+    {
+        public Cart Cart { get; set; }
+    }
+}
