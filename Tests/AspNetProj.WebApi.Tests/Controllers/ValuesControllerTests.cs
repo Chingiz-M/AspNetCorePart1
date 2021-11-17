6902da3a037029cae3caa3b4f8e6690ff6516301
Chingiz <chin1296@yandex.ru>
2021-11-17 22:17:23 +0300

    Пример интеграционного тестирования контроллера Values WebApi

---
+++ Tests/AspNetProj.WebApi.Tests/Controllers/ValuesControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using AspNetCoreProj.Interfaces;
+using AspNetCoreProj.WebApi;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace AspNetProj.WebApi.Tests.Controllers
+{
+    [TestClass]
+    public class ValuesControllerTests
+    {
+        private readonly WebApplicationFactory<Startup> _Host = new();
+
+        [TestMethod]
+        public async Task GetValues_IntegrityTest()
+        {
+            var client = _Host.CreateClient();
+
+            var response = await client.GetAsync(WebApiAddresses.Values); 
+
+            response.EnsureSuccessStatusCode();
+
+            var values = await response.Content.ReadFromJsonAsync<IEnumerable<string>>();
+        }
+    }
+}
